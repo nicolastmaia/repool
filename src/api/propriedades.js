d7b56823ab73b75a45b8d504f266a60003d3b02b
Nicolas <nicolasterramaia@gmail.com>
2021-03-23 16:57:54 -0300

    feat: WIP create anuncio and propriedade api

---
+++ src/api/propriedades.js
@@ -0,0 +1,27 @@
+import axios from './base';
+
+const resourceEndpoint = '/propriedades';
+
+export const getAll = async () => {
+  try {
+    const response = await axios.get(resourceEndpoint);
+    return response.data;
+  } catch (error) {
+    console.log(error.message);
+    throw error;
+  }
+};
+
+export const getOne = async (id) => {
+  try {
+    const response = await axios.get(`${resourceEndpoint}/${id}`);
+    return response.data;
+  } catch (error) {
+    console.log(error.message);
+    throw error;
+  }
+};
+
+export const post = async (anuncio) => {};
+export const edit = async (id, newData) => {};
+export const remove = async (id) => {};
