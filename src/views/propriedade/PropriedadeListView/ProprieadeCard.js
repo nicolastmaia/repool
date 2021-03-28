43928adf5df34c9aaaf3fae150eff88c3e7d959c
Nicolas <nicolasterramaia@gmail.com>
2021-03-28 12:16:10 -0300

    refact: change variables names; add special icons for pet, garage, etc; change card size

--- src/views/propriedade/PropriedadeListView/ProprieadeCard.js
+++ src/views/propriedade/PropriedadeListView/ProprieadeCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import {
@@ -12,51 +12,116 @@ import {
   makeStyles,
 } from '@material-ui/core';
 import StarRateIcon from '@material-ui/icons/StarRate';
+import { AvatarGroup, Rating } from '@material-ui/lab';
+import PetIcon from '@material-ui/icons/Pets';
+import PoolIcon from '@material-ui/icons/Pool';
+import GourmetAreaIcon from '@material-ui/icons/Fastfood';
+import CarIcon from '@material-ui/icons/DirectionsCar';
+import WifiIcon from '@material-ui/icons/Wifi';
+import { blue, green, orange, purple, red } from '@material-ui/core/colors';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     flexDirection: 'column',
   },
+  cardContentRoot: {
+    padding: 5,
+    paddingBottom: 16,
+  },
   statsItem: {
     alignItems: 'center',
     display: 'flex',
   },
-  statsIcon: {
-    marginRight: theme.spacing(1),
+  image: {
+    height: 150,
+    width: '100%',
+  },
+  petAvatar: {
+    height: 25,
+    width: 25,
+    color: '#fff',
+    backgroundColor: '#87C094',
+  },
+  poolAvatar: {
+    height: 25,
+    width: 25,
+    color: '#fff',
+    backgroundColor: '#76CFD0',
+  },
+  carAvatar: {
+    height: 25,
+    width: 25,
+    color: '#fff',
+    backgroundColor: '#DB6B6B',
+  },
+  wifiAvatar: {
+    height: 25,
+    width: 25,
+    color: '#fff',
+    backgroundColor: '#858DFF',
+  },
+  gourmetAvatar: {
+    height: 25,
+    width: 25,
+    color: '#fff',
+    backgroundColor: '#D9C06D',
   },
 }));
 
-const PropriedadeCard = ({ className, product, ...rest }) => {
+const PropriedadeCard = ({ className, propriedade, ...rest }) => {
   const classes = useStyles();
+  const [value, setValue] = useState(2);
 
   return (
     <Card className={clsx(classes.root, className)} {...rest}>
-      <CardContent>
-        <Box display='flex' justifyContent='center' mb={3}>
-          <Avatar alt='Product' src={product.media} variant='square' />
+      <CardContent classes={{ root: classes.cardContentRoot }}>
+        <Box position='relative' mb={3}>
+          <Avatar
+            alt='Propriedade'
+            src={propriedade.image}
+            className={classes.image}
+            variant='square'
+          />
+          <Box display='flex' position='absolute' right={0} bottom={0}>
+            <Avatar variant='square' className={classes.wifiAvatar}>
+              <WifiIcon style={{ fontSize: 15 }} />
+            </Avatar>
+            <Avatar variant='square' className={classes.petAvatar}>
+              <PetIcon style={{ fontSize: 15 }} />
+            </Avatar>
+            <Avatar variant='square' className={classes.gourmetAvatar}>
+              <GourmetAreaIcon style={{ fontSize: 15 }} />
+            </Avatar>
+            <Avatar variant='square' className={classes.carAvatar}>
+              <CarIcon style={{ fontSize: 15 }} />
+            </Avatar>
+            <Avatar variant='square' className={classes.poolAvatar}>
+              <PoolIcon style={{ fontSize: 15 }} />
+            </Avatar>
+          </Box>
         </Box>
+
         <Typography
           align='center'
           color='textPrimary'
           gutterBottom
           variant='h4'
         >
-          {product.title}
+          {propriedade.title}
         </Typography>
         <Typography align='center' color='textPrimary' variant='body1'>
-          {product.description}
+          {`${propriedade.city}, ${propriedade.state}`}
         </Typography>
       </CardContent>
       <Box flexGrow={1} />
       <Divider />
-      <Box p={2}>
+      <Box p={0.5}>
         <Grid container justify='space-between' spacing={2}>
           <Grid className={classes.statsItem} item>
-            <StarRateIcon className={classes.statsIcon} color='action' />
-            <Typography color='textSecondary' display='inline' variant='body2'>
-              {product.totalDownloads}
-            </Typography>
+            <Box component='fieldset' borderColor='transparent'>
+              <Rating name='read-only' value={value} readOnly />
+            </Box>
           </Grid>
         </Grid>
       </Box>
@@ -66,7 +131,7 @@ const PropriedadeCard = ({ className, product, ...rest }) => {
 
 PropriedadeCard.propTypes = {
   className: PropTypes.string,
-  product: PropTypes.object.isRequired,
+  propriedade: PropTypes.object.isRequired,
 };
 
 export default PropriedadeCard;
