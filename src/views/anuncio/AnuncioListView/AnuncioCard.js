ade2d33d2b2f14f36ea07c42764ed1660f3ad953
Nicolas <nicolasterramaia@gmail.com>
2021-03-28 12:14:39 -0300

    style: add special icons for pet, garage, etc; change rating icon; change card size

--- src/views/anuncio/AnuncioListView/AnuncioCard.js
+++ src/views/anuncio/AnuncioListView/AnuncioCard.js
@@ -11,28 +11,71 @@ import {
   Typography,
   makeStyles,
   IconButton,
+  GridListTileBar,
+  GridListTile,
 } from '@material-ui/core';
-import StarRateIcon from '@material-ui/icons/StarRate';
+import PetIcon from '@material-ui/icons/Pets';
+import PoolIcon from '@material-ui/icons/Pool';
+import GourmetAreaIcon from '@material-ui/icons/Fastfood';
+import CarIcon from '@material-ui/icons/DirectionsCar';
+import WifiIcon from '@material-ui/icons/Wifi';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import FavoriteIcon from '@material-ui/icons/Favorite';
+import { Rating } from '@material-ui/lab';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     flexDirection: 'column',
   },
+  cardContentRoot: {
+    padding: 5,
+    paddingBottom: 16,
+  },
   statsItem: {
     alignItems: 'center',
     display: 'flex',
   },
-  statsIcon: {
-    marginRight: theme.spacing(1),
+  image: {
+    height: 150,
+    width: '100%',
+  },
+  petAvatar: {
+    height: 25,
+    width: 25,
+    color: '#fff',
+    backgroundColor: '#87C094',
+  },
+  poolAvatar: {
+    height: 25,
+    width: 25,
+    color: '#fff',
+    backgroundColor: '#76CFD0',
+  },
+  carAvatar: {
+    height: 25,
+    width: 25,
+    color: '#fff',
+    backgroundColor: '#DB6B6B',
+  },
+  wifiAvatar: {
+    height: 25,
+    width: 25,
+    color: '#fff',
+    backgroundColor: '#858DFF',
+  },
+  gourmetAvatar: {
+    height: 25,
+    width: 25,
+    color: '#fff',
+    backgroundColor: '#D9C06D',
   },
 }));
 
 const AnuncioCard = ({ openConfirmDialog, className, anuncio, ...rest }) => {
   const classes = useStyles();
   const [isFavorite, setIsFavorite] = useState(anuncio.isFavorite);
+  const [value, setValue] = useState(2);
 
   const handleFavoritePress = () => {
     if (isFavorite) {
@@ -44,31 +87,53 @@ const AnuncioCard = ({ openConfirmDialog, className, anuncio, ...rest }) => {
 
   return (
     <Card className={clsx(classes.root, className)} {...rest}>
-      <CardContent>
-        <Box display='flex' justifyContent='center' mb={3}>
-          <Avatar alt='Anuncio' src={anuncio.image} variant='square' />
+      <CardContent classes={{ root: classes.cardContentRoot }}>
+        <Box position='relative' mb={3}>
+          <Avatar
+            alt='Anuncio'
+            src={anuncio.image}
+            className={classes.image}
+            variant='square'
+          />
+          <Box display='flex' position='absolute' right={0} bottom={0}>
+            <Avatar variant='square' className={classes.wifiAvatar}>
+              <WifiIcon style={{ fontSize: 15 }} />
+            </Avatar>
+            <Avatar variant='square' className={classes.petAvatar}>
+              <PetIcon style={{ fontSize: 15 }} />
+            </Avatar>
+            <Avatar variant='square' className={classes.gourmetAvatar}>
+              <GourmetAreaIcon style={{ fontSize: 15 }} />
+            </Avatar>
+            <Avatar variant='square' className={classes.carAvatar}>
+              <CarIcon style={{ fontSize: 15 }} />
+            </Avatar>
+            <Avatar variant='square' className={classes.poolAvatar}>
+              <PoolIcon style={{ fontSize: 15 }} />
+            </Avatar>
+          </Box>
         </Box>
+
         <Typography
           align='center'
           color='textPrimary'
           gutterBottom
           variant='h4'
         >
-          {anuncio.name}
+          {anuncio.title}
         </Typography>
         <Typography align='center' color='textPrimary' variant='body1'>
-          {anuncio.description}
+          {`${anuncio.city}, ${anuncio.state}`}
         </Typography>
       </CardContent>
       <Box flexGrow={1} />
       <Divider />
-      <Box p={2}>
+      <Box p={0.5}>
         <Grid container justify='space-between' spacing={2}>
           <Grid className={classes.statsItem} item>
-            <StarRateIcon className={classes.statsIcon} color='action' />
-            <Typography color='textSecondary' display='inline' variant='body2'>
-              {anuncio.rate}
-            </Typography>
+            <Box component='fieldset' borderColor='transparent'>
+              <Rating name='read-only' value={value} readOnly />
+            </Box>
           </Grid>
           <IconButton onClick={handleFavoritePress}>
             {isFavorite ? (
--- src/views/favorito/FavoritoListView/FavoritoCard.js
+++ src/views/favorito/FavoritoListView/FavoritoCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import {
@@ -12,25 +12,66 @@ import {
   makeStyles,
   IconButton,
 } from '@material-ui/core';
-import StarRateIcon from '@material-ui/icons/StarRate';
+import PetIcon from '@material-ui/icons/Pets';
+import PoolIcon from '@material-ui/icons/Pool';
+import GourmetAreaIcon from '@material-ui/icons/Fastfood';
+import CarIcon from '@material-ui/icons/DirectionsCar';
+import WifiIcon from '@material-ui/icons/Wifi';
 import FavoriteIcon from '@material-ui/icons/Favorite';
+import { Rating } from '@material-ui/lab';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     flexDirection: 'column',
   },
+  cardContentRoot: {
+    padding: 5,
+    paddingBottom: 16,
+  },
   statsItem: {
     alignItems: 'center',
     display: 'flex',
   },
-  statsIcon: {
-    marginRight: theme.spacing(1),
+  image: {
+    height: 150,
+    width: '100%',
+  },
+  petAvatar: {
+    height: 25,
+    width: 25,
+    color: '#fff',
+    backgroundColor: '#87C094',
+  },
+  poolAvatar: {
+    height: 25,
+    width: 25,
+    color: '#fff',
+    backgroundColor: '#76CFD0',
+  },
+  carAvatar: {
+    height: 25,
+    width: 25,
+    color: '#fff',
+    backgroundColor: '#DB6B6B',
+  },
+  wifiAvatar: {
+    height: 25,
+    width: 25,
+    color: '#fff',
+    backgroundColor: '#858DFF',
+  },
+  gourmetAvatar: {
+    height: 25,
+    width: 25,
+    color: '#fff',
+    backgroundColor: '#D9C06D',
   },
 }));
 
 const FavoritoCard = ({ openConfirmDialog, className, favorito, ...rest }) => {
   const classes = useStyles();
+  const [value, setValue] = useState(2);
 
   const handleFavoritePress = () => {
     openConfirmDialog(true);
@@ -38,10 +79,33 @@ const FavoritoCard = ({ openConfirmDialog, className, favorito, ...rest }) => {
 
   return (
     <Card className={clsx(classes.root, className)} {...rest}>
-      <CardContent>
-        <Box display='flex' justifyContent='center' mb={3}>
-          <Avatar alt='Favorito' src={favorito.media} variant='square' />
+      <CardContent classes={{ root: classes.cardContentRoot }}>
+        <Box position='relative' mb={3}>
+          <Avatar
+            alt='Favorito'
+            src={favorito.media}
+            className={classes.image}
+            variant='square'
+          />
+          <Box display='flex' position='absolute' right={0} bottom={0}>
+            <Avatar variant='square' className={classes.wifiAvatar}>
+              <WifiIcon style={{ fontSize: 15 }} />
+            </Avatar>
+            <Avatar variant='square' className={classes.petAvatar}>
+              <PetIcon style={{ fontSize: 15 }} />
+            </Avatar>
+            <Avatar variant='square' className={classes.gourmetAvatar}>
+              <GourmetAreaIcon style={{ fontSize: 15 }} />
+            </Avatar>
+            <Avatar variant='square' className={classes.carAvatar}>
+              <CarIcon style={{ fontSize: 15 }} />
+            </Avatar>
+            <Avatar variant='square' className={classes.poolAvatar}>
+              <PoolIcon style={{ fontSize: 15 }} />
+            </Avatar>
+          </Box>
         </Box>
+
         <Typography
           align='center'
           color='textPrimary'
@@ -51,18 +115,17 @@ const FavoritoCard = ({ openConfirmDialog, className, favorito, ...rest }) => {
           {favorito.title}
         </Typography>
         <Typography align='center' color='textPrimary' variant='body1'>
-          {favorito.description}
+          {`${favorito.city}, ${favorito.state}`}
         </Typography>
       </CardContent>
       <Box flexGrow={1} />
       <Divider />
-      <Box p={2}>
+      <Box p={0.5}>
         <Grid container justify='space-between' spacing={2}>
           <Grid className={classes.statsItem} item>
-            <StarRateIcon className={classes.statsIcon} color='action' />
-            <Typography color='textSecondary' display='inline' variant='body2'>
-              {favorito.rate}
-            </Typography>
+            <Box component='fieldset' borderColor='transparent'>
+              <Rating name='read-only' value={value} readOnly />
+            </Box>
           </Grid>
           <IconButton onClick={handleFavoritePress}>
             <FavoriteIcon color='action' />
