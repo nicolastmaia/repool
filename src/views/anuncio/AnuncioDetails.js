004243cbd9d9bc90efb1aff416882d772f2b99d8
Nicolas <nicolasterramaia@gmail.com>
2021-03-31 08:54:20 -0300

    feat: WIA building AnuncioDetail page

--- src/views/anuncio/AnuncioDetails.js
+++ src/views/anuncio/AnuncioDetails.js
@@ -11,145 +11,338 @@ import {
   Grid,
   TextField,
   makeStyles,
+  Container,
+  Typography,
+  IconButton,
+  GridList,
+  GridListTile,
 } from '@material-ui/core';
 import AuthContext from 'src/contexts/AuthContext';
+import PetIcon from '@material-ui/icons/PetsOutlined';
+import PoolIcon from '@material-ui/icons/PoolOutlined';
+import GourmetAreaIcon from '@material-ui/icons/FastfoodOutlined';
+import CarIcon from '@material-ui/icons/DirectionsCarOutlined';
+import WifiIcon from '@material-ui/icons/WifiOutlined';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import {
+  ExpandLess as ExpandLessIcon,
+  ExpandMore as ExpandMoreIcon,
+} from '@material-ui/icons';
+import ShowMoreText from 'react-show-more-text';
 
-const states = [
+import { Rating } from '@material-ui/lab';
+
+const tileData = [
   {
-    value: 'alabama',
-    label: 'Alabama',
+    img: 'https://picsum.photos/500/300?random=1',
+    title: 'Breakfast',
+    author: 'jill111',
+    cols: 2,
+    featured: true,
   },
   {
-    value: 'new-york',
-    label: 'New York',
+    img: 'https://picsum.photos/500/300?random=2',
+    title: 'Tasty burger',
+    author: 'director90',
   },
   {
-    value: 'san-francisco',
-    label: 'San Francisco',
+    img: 'https://picsum.photos/500/300?random=3',
+    title: 'Camera',
+    author: 'Danson67',
+  },
+  {
+    img: 'https://picsum.photos/500/300?random=4',
+
+    title: 'Morning',
+    author: 'fancycrave1',
+    featured: true,
+  },
+  {
+    img: 'https://picsum.photos/500/300?random=5',
+
+    title: 'Hats',
+    author: 'Hans',
+  },
+  {
+    img: 'https://picsum.photos/500/300?random=6',
+    title: 'Honey',
+    author: 'fancycravel',
+  },
+  {
+    img: 'https://picsum.photos/500/300?random=7',
+    title: 'Vegetables',
+    author: 'jill111',
+    cols: 2,
+  },
+  {
+    img: 'https://picsum.photos/500/300?random=8',
+    title: 'Water plant',
+    author: 'BkrmadtyaKarki',
+  },
+  {
+    img: 'https://picsum.photos/500/300?random=9',
+    title: 'Mushrooms',
+    author: 'PublicDomainPictures',
+  },
+  {
+    img: 'https://picsum.photos/500/300?random=10',
+    title: 'Olive oil',
+    author: 'congerdesign',
+  },
+  {
+    img: 'https://picsum.photos/500/300?random=11',
+    title: 'Sea star',
+    cols: 2,
+    author: '821292',
+  },
+  {
+    img: 'https://picsum.photos/500/300?random=12',
+    title: 'Bike',
+    author: 'danfador',
   },
 ];
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   root: {},
+  gridListContainer: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: 'transparent',
+  },
+  gridList: {
+    maxHeight: 350,
+  },
+  titleContainer: {
+    paddingTop: '2em',
+    paddingBottom: '2em',
+  },
+  descriptionContainer: {
+    paddingTop: '3em',
+    paddingBottom: '3em',
+  },
+  favoriteText: {
+    marginLeft: '5%',
+  },
+  titleText: {
+    marginBottom: '0.5em',
+  },
+  featureContainer: {
+    paddingTop: '3em',
+    paddingBottom: '3em',
+  },
+  featureItem: {
+    display: 'flex',
+  },
+  petIcon: {
+    color: '#4D935E',
+  },
+  poolIcon: {
+    color: '#39A5A7',
+  },
+  carIcon: {
+    color: '#B52C2C',
+  },
+  wifiIcon: {
+    color: '#858DFF',
+  },
+  gourmetIcon: {
+    color: '#B2942E',
+  },
 }));
 
 const AnuncioDetails = ({ className, ...rest }) => {
   const classes = useStyles();
-  const { user } = useContext(AuthContext);
+  const [isFavorite, setIsFavorite] = useState(false);
+  const [value, setValue] = useState(2);
+  const [expand, setExpand] = useState(false);
 
-  const [values, setValues] = useState({
-    firstName: user.firstName,
-    lastName: user.lastName,
-    email: user.email,
-    phone: user.phone,
-    state: 'Alabama',
-    country: 'USA',
-  });
+  const handleFavoritePress = () => {
+    if (isFavorite) {
+      return true;
+    }
+    return false;
+  };
 
-  const handleChange = (event) => {
-    setValues({
-      ...values,
-      [event.target.name]: event.target.value,
-    });
+  const toggleExpand = () => {
+    setExpand((prevState) => !prevState);
   };
 
   return (
-    <form
-      autoComplete='off'
-      noValidate
-      className={clsx(classes.root, className)}
-      {...rest}
-    >
-      <Card>
-        <CardHeader subheader='The information can be edited' title='Profile' />
-        <Divider />
-        <CardContent>
-          <Grid container spacing={3}>
-            <Grid item md={6} xs={12}>
-              <TextField
-                fullWidth
-                helperText='Please specify the first name'
-                label='First name'
-                name='firstName'
-                onChange={handleChange}
-                required
-                value={values.firstName}
-                variant='outlined'
-              />
-            </Grid>
-            <Grid item md={6} xs={12}>
-              <TextField
-                fullWidth
-                label='Last name'
-                name='lastName'
-                onChange={handleChange}
-                required
-                value={values.lastName}
-                variant='outlined'
-              />
-            </Grid>
-            <Grid item md={6} xs={12}>
-              <TextField
-                fullWidth
-                label='Email Address'
-                name='email'
-                onChange={handleChange}
-                required
-                value={values.email}
-                variant='outlined'
-              />
-            </Grid>
-            <Grid item md={6} xs={12}>
-              <TextField
-                fullWidth
-                label='Phone Number'
-                name='phone'
-                onChange={handleChange}
-                type='number'
-                value={values.phone}
-                variant='outlined'
-              />
-            </Grid>
-            <Grid item md={6} xs={12}>
-              <TextField
-                fullWidth
-                label='Country'
-                name='country'
-                onChange={handleChange}
-                required
-                value={values.country}
-                variant='outlined'
-              />
-            </Grid>
-            <Grid item md={6} xs={12}>
-              <TextField
-                fullWidth
-                label='Select State'
-                name='state'
-                onChange={handleChange}
-                required
-                select
-                SelectProps={{ native: true }}
-                value={values.state}
-                variant='outlined'
-              >
-                {states.map((option) => (
-                  <option key={option.value} value={option.value}>
-                    {option.label}
-                  </option>
-                ))}
-              </TextField>
-            </Grid>
-          </Grid>
-        </CardContent>
-        <Divider />
-        <Box display='flex' justifyContent='flex-end' p={2}>
-          <Button color='primary' variant='contained'>
-            Save details
-          </Button>
+    <Container className={classes.root}>
+      <Container className={classes.titleContainer}>
+        <Box>
+          <Typography className={classes.titleText} variant='h1'>
+            Quarto com banheiro na Paulista
+          </Typography>
         </Box>
-      </Card>
-    </form>
+        <Box>
+          <Grid
+            container
+            justify='space-between'
+            alignItems='center'
+            spacing={2}
+          >
+            <Grid item>
+              <Rating name='read-only' value={value} readOnly />
+            </Grid>
+            <IconButton onClick={handleFavoritePress}>
+              {isFavorite ? (
+                <>
+                  <FavoriteIcon color='action' />
+                  <Typography className={classes.favoriteText} variant='h4'>
+                    Desfavoritar
+                  </Typography>
+                </>
+              ) : (
+                <>
+                  <FavoriteBorderIcon color='action' />
+                  <Typography className={classes.favoriteText} variant='h4'>
+                    Favoritar
+                  </Typography>
+                </>
+              )}
+            </IconButton>
+          </Grid>
+        </Box>
+      </Container>
+      <Container className={classes.gridListContainer}>
+        <GridList cellHeight={160} className={classes.gridList} cols={3}>
+          {tileData.map((tile) => (
+            <GridListTile key={tile.img} cols={tile.cols || 1}>
+              <img src={tile.img} alt={tile.title} />
+            </GridListTile>
+          ))}
+        </GridList>
+      </Container>
+      <Container className={classes.descriptionContainer}>
+        <Box>
+          <Typography variant='h2' className={classes.titleText}>
+            <ShowMoreText
+              lines={1}
+              more={<ExpandMoreIcon />}
+              less={<ExpandLessIcon />}
+              onClick={toggleExpand}
+              expanded={expand}
+              width={500}
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+              eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor
+              massa. Pellentesque habitant morbi tristique senectus et netus et
+              malesuada fames ac turpis egestas. Nulla at risus. Quisque purus
+              magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis,
+              felis ut adipiscing
+            </ShowMoreText>
+          </Typography>
+        </Box>
+        <Box>
+          <Typography variant='h4'>2 vagas</Typography>
+        </Box>
+      </Container>
+
+      <Divider />
+
+      <Container className={classes.featureContainer}>
+        <Grid container direction='column' justify='space-evenly' spacing={4}>
+          <Grid item>
+            <Typography gutterBottom={2} variant='h2'>
+              Comodidades
+            </Typography>
+          </Grid>
+          <Grid item className={classes.featureItem}>
+            <Box>
+              <PetIcon
+                fontSize='large'
+                color='primary'
+                className={classes.petIcon}
+              />
+            </Box>
+            <Box ml={3}>
+              <Box mb={1}>
+                <Typography variant='h4'>Traga seu pet</Typography>
+              </Box>
+              <Box>
+                <Typography variant='body1'>Descrição da feature</Typography>
+              </Box>
+            </Box>
+          </Grid>
+          <Grid item className={classes.featureItem}>
+            <Box>
+              <PoolIcon
+                fontSize='large'
+                color='primary'
+                className={classes.poolIcon}
+              />
+            </Box>
+            <Box ml={3}>
+              <Box mb={1}>
+                <Typography variant='h4'>Tem piscina</Typography>
+              </Box>
+              <Box>
+                <Typography variant='body1'>Descrição da feature</Typography>
+              </Box>
+            </Box>
+          </Grid>
+          <Grid item className={classes.featureItem}>
+            <Box>
+              <CarIcon
+                fontSize='large'
+                color='primary'
+                className={classes.carIcon}
+              />
+            </Box>
+            <Box ml={3}>
+              <Box mb={1}>
+                <Typography variant='h4'>Tem garagem</Typography>
+              </Box>
+              <Box>
+                <Typography variant='body1'>Descrição da feature</Typography>
+              </Box>
+            </Box>
+          </Grid>
+          <Grid item className={classes.featureItem}>
+            <Box>
+              <WifiIcon
+                fontSize='large'
+                color='primary'
+                className={classes.wifiIcon}
+              />
+            </Box>
+            <Box ml={3}>
+              <Box mb={1}>
+                <Typography variant='h4'>Tem internet wifi</Typography>
+              </Box>
+              <Box>
+                <Typography variant='body1'>Descrição da feature</Typography>
+              </Box>
+            </Box>
+          </Grid>
+          <Grid item className={classes.featureItem}>
+            <Box>
+              <GourmetAreaIcon
+                fontSize='large'
+                color='primary'
+                className={classes.gourmetIcon}
+              />
+            </Box>
+            <Box ml={3}>
+              <Box mb={1}>
+                <Typography variant='h4'>Tem área gourmet</Typography>
+              </Box>
+              <Box>
+                <Typography variant='body1'>Descrição da feature</Typography>
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+      <Divider />
+      <Container className={classes.titleContainer}>
+        <Box>comentarios e avaliaçao</Box>
+      </Container>
+    </Container>
   );
 };
 
